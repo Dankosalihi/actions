472124f71e4cffb98eaa642642b9e2e362ad8d5f
Dankosalihi <danijelsalihi@hotmail.com>
2024-09-30 10:52:20 +0200

    Change text to Home 2.0

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      <Text>Home 2.0</Text>
       <StatusBar style="auto" />
     </View>
   );
